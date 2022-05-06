a5cff5dcab66b417ed9e95be1c9648d5b281f30e
NataliiaDyachok <nataladyachok@gmail.com>
2022-05-06 15:48:13 +0300

    fixed models account and token

--- src/server/db/sequelize/models/token.js
+++ src/server/db/sequelize/models/token.js
@@ -25,7 +25,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       timestamps: false,
       underscored: false,
-      tableName: 'token',
+      tableName: 'tokens',
       indexes: [{ fields: ['title'] }],
     },
   );
