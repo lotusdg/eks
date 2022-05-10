fcaa15947114940b1bcd55a2eb30e5cb66717bdd
NataliiaDyachok <nataladyachok@gmail.com>
2022-05-10 12:37:29 +0300

    HTTP status codes

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,9 +14,11 @@ function fatal(message) {
 const httpCodes = {
   ok: 200,
   badReq: 400,
-  notFound: 404,
   unauthorized: 401,
+  forbidden: 403,
+  notFound: 404,
   serverError: 500,
+  notImplemented: 501,
 };
 
 function resFinish(res, code, message) {
